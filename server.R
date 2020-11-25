f52c4efa0bcb83b2a187669a33e46524582f7ac6
ys634 <yutaro.sakairi@gmail.com>
2020-11-25 13:47:29 -0800

    changed ggplot to plotly

--- server.R
+++ server.R
@@ -1,12 +1,35 @@
 #Plant phenology
+source("cicerone.R", local = T)
 
 # Load data
 clim.dat = read.csv("Ex2_climatedata.csv")
 phen.dat=read.csv("Ex2_phenologydata_long.csv")
+specs <- c("Amelanchier canadensis", "Cichorium intybus", "Erigeron canadensis", "Erigeron pulchellus", "Vaccinium corymbosum")
 
+for (i in c(1:123)) {
+  clim.dat$JanApr[i] <- mean(c(clim.dat$Jan[i], clim.dat$Feb[i], clim.dat$Mar[i], clim.dat$Apr[i]), na.rm = T)
+}
+
+for (i in c(1:75)) {
+  phen.dat$JanApr[i] <- mean(c(phen.dat$Jan[i], phen.dat$Feb[i], phen.dat$Mar[i], phen.dat$Apr[i]), na.rm = T)
+}
 
 # Define server logic to do filtering
 shinyServer(function(input, output) {
+  
+  observeEvent(input$tour1, guide1$init()$start())
+  
+  observeEvent(input$reset1, {
+    reset("month-wrapper")
+    reset("year-wrapper")
+  })
+  
+  observeEvent(input$tour2, guide2$init()$start())
+  
+  observeEvent(input$reset2, {
+    reset("month")
+    reset("species")
+  })
   
   clim.data <- reactive({
     #restrict years
@@ -16,7 +39,7 @@ shinyServer(function(input, output) {
   
   #pick species
   phen.data <- reactive({
-    phen.dat %>% filter(Species %in% input$species.sel)
+    phen.dat %>% filter(Species %in% input$species)
   })
   
   
@@ -38,19 +61,63 @@ shinyServer(function(input, output) {
       pval <- paste("<b style = 'color:red;'>", pval, "</b>")
     }
     
-    HTML("<b>Trend line analysis</b>
-         <br>Slope:", signif(as.numeric(fit$coefficients[2]), digits = 2), 
+    HTML("<b>Stats</b>
+         <br>Slope:", signif(as.numeric(fit$coefficients[2]), digits = 2), "°C/year",
          "<br> p-value:", pval)
   })
   
   output$PhenologyPlot <- renderPlot({
-    ggplot(data = phen.data(), aes_string(x = input$x, y = 'doy')) +
+    ggplot(data = phen.data(), aes_string(x = input$month, y = 'doy')) +
       geom_point() +
       geom_smooth(method = "lm", se = F) +
       facet_wrap(~Species, ncol = 2, scales = "free") +
       theme_bw() + ylab("First flowring date (day of year)") + xlab("Temperature (°C)") +
       theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12), 
             legend.text = element_text(size = 12), legend.title = element_text(size = 12))
+  })
+  
+  
+  output$PhenologyPlotly <- renderPlotly({
+    validate(
+      need(input$species, "Select species")
+    )
+    df <- phen.data()
+    xvar <- df[, input$month]
+    
+    pal <- c('#d7191c', '#fdae61', 'black', 'green', '#2c7bb6')
+    pal <- setNames(pal, specs)  
+    p <- plot_ly() %>%
+      add_markers(x = ~xvar, 
+                  y = ~df$doy, 
+                  color = ~df$Species, 
+                  colors = pal, 
+                  type = "scatter",
+                  text = paste0("<br>Year: ", df[, "year"]),
+                  hovertemplate = "(%{x}, %{y}) %{text}") %>%
+      layout(xaxis = list(title = "Temperature (°C)"),
+             yaxis = list(title = "First flowring date (day of year)"))
+    
+    array <- input$species
+    for (i in 1:length(array)) {
+      species <- array[i]
+      subset <- df[df$Species == species, ][, c("year", "doy", input$month, "Species")] %>% na.omit()
+      fit <- lm(subset[, "doy"] ~ subset[, input$month])
+
+      if (dim(summary(fit)$coefficients) > 1) {
+        linetype <- ifelse(signif(summary(fit)$coefficients[2,4], digits = 2) < 0.05, "solid", "dash")
+        
+        p <- p %>% 
+          add_lines(x = subset[, input$month], 
+                    y = fitted(fit), 
+                    color = subset[, "Species"], 
+                    colors = pal, 
+                    name = paste(species, "trendline"), 
+                    line = list(dash = linetype), 
+                    mode = "lines")
+      }
+    }
+
+    p
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,16 @@
 # Plant phenology
 
 library(shiny)
+library(ggplot2)
+library(magrittr)
 library(tidyverse)
 library(stringr)
-library(cowplot)
+library(shinyWidgets)
+library(cicerone)
+library(shinyjs)
+library(shinyBS)
+#library(cowplot)
+library(plotly)
 
 #specify choices
 specs <- c("Amelanchier canadensis", "Cichorium intybus", "Erigeron canadensis", "Erigeron pulchellus", "Vaccinium corymbosum")
@@ -16,20 +23,49 @@ clim.dat = read.csv("Ex2_climatedata.csv")
 shinyUI(
 
 # Define UI
-fluidPage(  
+fluidPage(id = "page",
+  use_cicerone(),
+  useShinyjs(),
   title = "Plant phenology",
 
   includeMarkdown("include.md"),
-
+  
+  actionBttn(
+    inputId = "reset1",
+    label = "Reset", 
+    style = "material-flat",
+    color = "danger",
+    size = "xs"
+  ),
+  bsTooltip("reset1", "If you have already changed the variables, reset them to default here before starting the tour."),
+  
+  actionBttn(
+    inputId = "tour1",
+    label = "Take a tour!", 
+    style = "material-flat",
+    color = "success",
+    size = "sm"
+  ), 
   hr(),
   # Pick options for climate plot
   fluidRow(
-     column(4,selectInput('clim.month', 'Month', choices = months)),
-     column(4,sliderInput('year', 'Range of years to plot', 
-                          min = min(clim.dat$Year),
+    
+    column(4, 
+           div(
+             id = "month-wrapper",
+             pickerInput('clim.month', 'Month', choices = months)
+           )
+    ),
+  
+    column(4, 
+           div(
+             id = "year-wrapper",
+             sliderInput('year', 'Range of years', min = min(clim.dat$Year),
                           max = max(clim.dat$Year), 
                           value = c(min(clim.dat$Year), max(clim.dat$Year)),
-                          format = "####",sep = "",step = 1))
+                          format = "####", sep = "", step = 1)
+           )
+    )
   ),
  
   # Climate plot
@@ -39,14 +75,49 @@ fluidPage(
   
   hr(),
   includeMarkdown("include2.md"),
-  
-  fluidRow(
-    column(4,selectInput('x', 'Climate Variable', choices = months)),
-    column(4,selectInput('species.sel', 'Select species to plot', choices = specs, multiple = TRUE, selected = specs))
+  br(),
+  actionBttn(
+    inputId = "reset2",
+    label = "Reset", 
+    style = "material-flat",
+    color = "danger",
+    size = "xs"
   ),
+  bsTooltip("reset2", "If you have already changed the variables, reset them to default here before starting the tour."),
   
-  plotOutput(outputId="PhenologyPlot"),
-  hr()
-  
+  actionBttn(
+    inputId = "tour2",
+    label = "Take a tour!", 
+    style = "material-flat",
+    color = "success",
+    size = "sm"
+  ), 
+  hr(),
+  div(
+    id = "viz-wrapper",
+
+    fluidRow(
+      column(4, 
+             div(
+               id = "month-wrapper", 
+               pickerInput('month', 'Month', choices = months)
+             )
+      ),
+      column(4, 
+             div(
+               id = "spec-wrapper", 
+               pickerInput('species', 'Species', choices = specs, multiple = TRUE, selected = specs[1],
+                           options = list(`actions-box` = TRUE))
+             )
+      )
+    ),
+    
+    # h4("ggplot"),
+    # plotOutput(outputId = "PhenologyPlot"),
+    # hr(),
+    # h4("Plotly"),
+    plotlyOutput(outputId = "PhenologyPlotly"),
+    hr()
+    )
   )
 )
